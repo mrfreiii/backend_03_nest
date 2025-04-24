e86f29040e342e22840fbff6e9163259993033d6
Igor_Titov <mrfreiii@gmail.com>
2025-04-24 19:04:57 +0300

    updated swagger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { config } from "dotenv";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
+// import { ServeStaticModule } from "@nestjs/serve-static";
+// import { join } from "path";
 
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { CoreModule } from "./core/core.module";
-import { GLOBAL_PREFIX } from "./setup/global-prefix.setup";
+// import { GLOBAL_PREFIX } from "./setup/global-prefix.setup";
 import { TestingModule } from "./modules/testing/testing.module";
 import { UserAccountsModule } from "./modules/user-accounts/user-accounts.module";
 import { BloggersPlatformModule } from "./modules/bloggers-platform/bloggers-platform.module";
@@ -17,13 +17,13 @@ config();
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "swagger-static"),
-      serveRoot:
-        process.env.NODE_ENV === "development"
-          ? "/"
-          : "/swagger",
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, "..", "swagger-static"),
+    //   serveRoot:
+    //     process.env.NODE_ENV === "development"
+    //       ? "/"
+    //       : "/swagger",
+    // }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL!, {
       dbName: process.env?.MONGO_DB_NAME,
--- src/setup/swagger.setup.ts
+++ src/setup/swagger.setup.ts
@@ -1,11 +1,11 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { createWriteStream } from "fs";
-import { get } from "http";
+// import { createWriteStream } from "fs";
+// import { get } from "http";
 
-import { SETTINGS } from "../settings";
+// import { SETTINGS } from "../settings";
 
-const serverUrl = `http://localhost:${SETTINGS.PORT}`;
+// const serverUrl = `http://localhost:${SETTINGS.PORT}`;
 
 export function swaggerSetup(app: INestApplication) {
   const config = new DocumentBuilder()
@@ -17,42 +17,52 @@ export function swaggerSetup(app: INestApplication) {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("swagger", app, document, {
     customSiteTitle: "Swagger site title",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
   });
 
   // get the swagger json file (if app is running in development mode)
-  if (process.env.NODE_ENV === "development") {
-    // write swagger ui files
-    get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
-      response.pipe(createWriteStream("swagger-static/swagger-ui-bundle.js"));
-      console.log(
-        `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
-      );
-    });
-
-    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
-      response.pipe(createWriteStream("swagger-static/swagger-ui-init.js"));
-      console.log(
-        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
-      );
-    });
-
-    get(
-      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream("swagger-static/swagger-ui-standalone-preset.js"),
-        );
-        console.log(
-          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
-        );
-      },
-    );
-
-    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
-      response.pipe(createWriteStream("swagger-static/swagger-ui.css"));
-      console.log(
-        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
-      );
-    });
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   // write swagger ui files
+  //   get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+  //     response.pipe(createWriteStream("swagger-static/swagger-ui-bundle.js"));
+  //     console.log(
+  //       `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+  //     );
+  //   });
+  //
+  //   get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+  //     response.pipe(createWriteStream("swagger-static/swagger-ui-init.js"));
+  //     console.log(
+  //       `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+  //     );
+  //   });
+  //
+  //   get(
+  //     `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+  //     function (response) {
+  //       response.pipe(
+  //         createWriteStream("swagger-static/swagger-ui-standalone-preset.js"),
+  //       );
+  //       console.log(
+  //         `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+  //       );
+  //     },
+  //   );
+  //
+  //   get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+  //     response.pipe(createWriteStream("swagger-static/swagger-ui.css"));
+  //     console.log(
+  //       `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+  //     );
+  //   });
+  // }
 }
