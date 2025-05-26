28dbb0e53ec0fd5766099baea4f353a1e5bdf81c
Igor_Titov <mrfreiii@gmail.com>
2025-05-26 16:24:53 +0300

    updated blogs sort directions

--- src/core/dto/base.query-params.input-dto.ts
+++ src/core/dto/base.query-params.input-dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { IsEnum, IsNumber, IsOptional } from "class-validator";
+import { IsEnum, IsNumber } from "class-validator";
 
 export enum SortDirection {
   Asc = "asc",
