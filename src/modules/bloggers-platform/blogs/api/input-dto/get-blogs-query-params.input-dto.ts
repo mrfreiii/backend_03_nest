9bcf7893a443ed41d374a900948f9354ceb34ea6
Igor_Titov <mrfreiii@gmail.com>
2025-05-26 16:52:23 +0300

    updated creating blog validation

--- src/modules/bloggers-platform/blogs/api/input-dto/get-blogs-query-params.input-dto.ts
+++ src/modules/bloggers-platform/blogs/api/input-dto/get-blogs-query-params.input-dto.ts
@@ -1,10 +1,11 @@
-import { IsEnum, IsOptional, IsString } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 import { BlogsSortBy } from "./blogs-sort-by";
 import { BaseQueryParams } from "../../../../../core/dto/base.query-params.input-dto";
 
 export class GetBlogsQueryParams extends BaseQueryParams {
-  @IsEnum(BlogsSortBy)
+  @IsOptional()
+  // @IsEnum(BlogsSortBy)
   sortBy = BlogsSortBy.CreatedAt;
 
   @IsOptional()
